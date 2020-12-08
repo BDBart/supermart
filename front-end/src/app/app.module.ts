15130e081013c7dec10ecc8deaf3c8010537a470
[BartGeest] <[kkcbart@live.nl]>
2020-12-08 16:16:19 +0100

    added homepage component still work in progress

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Route, RouterModule} from "@angular/router";
+import { HomepageComponent } from './components/homepage/homepage.component';
+
+const routes: Route[] = [
+  { path: '', component: HomepageComponent },
+  { path: 'home', component: HomepageComponent }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
